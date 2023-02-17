900de418c6de408252264de122e39e0b19732885
wuqingjian <wuqingjiancpp@qq.com>
2023-02-17 16:19:17 +0800

    init custom user

--- web/src/main/resources/static/js/index/index.js
+++ web/src/main/resources/static/js/index/index.js
@@ -2,3 +2,7 @@ $(".nav-link").click(function () {
     $(".nav-link").removeClass("active");
     $(this).addClass("active");
 });
+$.getJSON( "/user", function( data ) {
+    let username = data.username;
+    $( "#username" ).html(username);
+});
