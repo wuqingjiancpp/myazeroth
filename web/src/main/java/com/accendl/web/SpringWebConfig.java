eae2c5af1a919f7bd9be75c619b714ac287e9af3
wuqingjian <wuqingjiancpp@qq.com>
2023-04-02 22:47:30 +0800

    init spring session redis
    Signed-off-by: wuqingjian <wuqingjiancpp@qq.com>

--- web/src/main/java/com/accendl/web/SpringWebConfig.java
+++ web/src/main/java/com/accendl/web/SpringWebConfig.java
@@ -1,7 +1,17 @@
 package com.accendl.web;
 
+import io.lettuce.core.api.StatefulConnection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClusterConnection;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionProvider;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.datetime.DateFormatter;
@@ -11,10 +21,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.text.DateFormat;
+import java.util.concurrent.CompletionStage;
 
 @Configuration(proxyBeanMethods = false)
 public class SpringWebConfig  {
@@ -36,6 +48,28 @@ public class SpringWebConfig  {
         };
     }
 
+//    @Bean
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
+//        redisStandaloneConfiguration.setPassword("1234");
+//        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+//    }
 
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(new RedisClusterConfiguration(clusterProperties.getNodes()));
+    }
+
+    /**
+     * Type safe representation of application.properties
+     */
+    @Autowired
+    private RedisClusterConfigurationProperties clusterProperties;
+
+//    public @Bean RedisConnectionFactory connectionFactory() {
+//
+//        return new JedisConnectionFactory(
+//                new RedisClusterConfiguration(clusterProperties.getNodes()));
+//    }
 
 }
--- web/src/main/java/com/accendl/web/security/customuser/UserController.java
+++ web/src/main/java/com/accendl/web/security/customuser/UserController.java
@@ -15,6 +15,8 @@
  */
 package com.accendl.web.security.customuser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
